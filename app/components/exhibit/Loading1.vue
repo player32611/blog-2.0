5d261466dd3c368da0786636a039711f5b113272
player32611 <hcb326630@163.com>
2026-04-26 18:36:55 +0800

    音乐加载优化

--- app/components/exhibit/Loading1.vue
+++ app/components/exhibit/Loading1.vue
@@ -13,12 +13,13 @@
 </template>
 
 <style scoped lang="scss">
+$base-size: 1;
+
 .loader {
 	position: relative;
-	width: 200px;
-	height: 200px;
+	width: 200px * $base-size;
+	height: 200px * $base-size;
 	border-radius: 50%;
-
 	background: radial-gradient(circle, rgba(255, 255, 255, 0.05) 30%, transparent 70%);
 	overflow: hidden;
 
@@ -125,6 +126,194 @@
 	}
 	100% {
 		transform: rotate(360deg);
+	}
+}
+
+/* ========== 超小屏（< 576px）========== */
+@media screen and (max-width: 576px) {
+	$base-size: 0.7;
+
+	.loader {
+		width: 200px * $base-size;
+		height: 200px * $base-size;
+
+		&::before {
+			border: 4px * $base-size solid transparent;
+		}
+
+		&::after {
+			filter: blur(2px * $base-size);
+		}
+
+		.loader__inner {
+			width: 30px * $base-size;
+			height: 30px * $base-size;
+			box-shadow: 0 0 15px * $base-size rgba(255, 255, 255, 0.6);
+		}
+
+		.loader__orbit {
+			.loader__dot {
+				width: 8px * $base-size;
+				height: 8px * $base-size;
+
+				&:nth-child(1) {
+					transform: rotate(0deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(2) {
+					transform: rotate(90deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(3) {
+					transform: rotate(180deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(4) {
+					transform: rotate(270deg) translate(60px * $base-size);
+				}
+			}
+		}
+	}
+}
+
+/* ========== 小屏（576px - 768px）========== */
+@media screen and (min-width: 576px) and (max-width: 768px) {
+	$base-size: 0.8;
+
+	.loader {
+		width: 200px * $base-size;
+		height: 200px * $base-size;
+
+		&::before {
+			border: 4px * $base-size solid transparent;
+		}
+
+		&::after {
+			filter: blur(2px * $base-size);
+		}
+
+		.loader__inner {
+			width: 30px * $base-size;
+			height: 30px * $base-size;
+			box-shadow: 0 0 15px * $base-size rgba(255, 255, 255, 0.6);
+		}
+
+		.loader__orbit {
+			.loader__dot {
+				width: 8px * $base-size;
+				height: 8px * $base-size;
+
+				&:nth-child(1) {
+					transform: rotate(0deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(2) {
+					transform: rotate(90deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(3) {
+					transform: rotate(180deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(4) {
+					transform: rotate(270deg) translate(60px * $base-size);
+				}
+			}
+		}
+	}
+}
+
+/* ========== 中等屏（768px - 991px）========== */
+@media screen and (min-width: 768px) and (max-width: 991px) {
+	$base-size: 0.9;
+
+	.loader {
+		width: 200px * $base-size;
+		height: 200px * $base-size;
+
+		&::before {
+			border: 4px * $base-size solid transparent;
+		}
+
+		&::after {
+			filter: blur(2px * $base-size);
+		}
+
+		.loader__inner {
+			width: 30px * $base-size;
+			height: 30px * $base-size;
+			box-shadow: 0 0 15px * $base-size rgba(255, 255, 255, 0.6);
+		}
+
+		.loader__orbit {
+			.loader__dot {
+				width: 8px * $base-size;
+				height: 8px * $base-size;
+
+				&:nth-child(1) {
+					transform: rotate(0deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(2) {
+					transform: rotate(90deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(3) {
+					transform: rotate(180deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(4) {
+					transform: rotate(270deg) translate(60px * $base-size);
+				}
+			}
+		}
+	}
+}
+
+/* ========== 大屏（991px - 1199px）========== */
+@media screen and (min-width: 991px) and (max-width: 1199px) {
+	$base-size: 1;
+
+	.loader {
+		width: 200px * $base-size;
+		height: 200px * $base-size;
+
+		&::before {
+			border: 4px * $base-size solid transparent;
+		}
+
+		&::after {
+			filter: blur(2px * $base-size);
+		}
+
+		.loader__inner {
+			width: 30px * $base-size;
+			height: 30px * $base-size;
+			box-shadow: 0 0 15px * $base-size rgba(255, 255, 255, 0.6);
+		}
+
+		.loader__orbit {
+			.loader__dot {
+				width: 8px * $base-size;
+				height: 8px * $base-size;
+
+				&:nth-child(1) {
+					transform: rotate(0deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(2) {
+					transform: rotate(90deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(3) {
+					transform: rotate(180deg) translate(60px * $base-size);
+				}
+
+				&:nth-child(4) {
+					transform: rotate(270deg) translate(60px * $base-size);
+				}
+			}
+		}
 	}
 }
 </style>
