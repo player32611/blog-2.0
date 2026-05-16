639f71bc5bc363607297ec518395cd1879976f88
player32611 <hcb326630@163.com>
2026-05-16 19:24:37 +0800

    字符动画

---
+++ app/components/ui/detailUI/DetailTitle.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
+const split = ref<SplitText | null>(null);
+
+const initDelay: number = 1; // 初始延迟（s）
+const singleDuration: number = 1.5; // 单字符动画时长（s）
+
+const firstCharAnim = () => {
+	if (!split.value || !split.value.chars[0]) return;
+	gsap.fromTo(
+		split.value.chars[0],
+		{ x: -300 },
+		{ x: 0, duration: singleDuration, ease: "power1.out", delay: initDelay },
+	);
+	gsap.fromTo(
+		split.value.chars[0],
+		{ y: -600 },
+		{ y: 0, duration: singleDuration, ease: "bounce.out", delay: initDelay },
+	);
+	gsap.fromTo(
+		split.value.chars[0],
+		{ rotate: 240 },
+		{ rotate: 720, duration: singleDuration, ease: "back.out", delay: initDelay },
+	);
+};
+
+const secondCharAnim = () => {
+	if (!split.value || !split.value.chars[1]) return;
+	gsap
+		.timeline()
+		.set(split.value.chars[1], { rotateX: -90 })
+		.to(split.value.chars[1], {
+			rotateX: -180,
+			transformOrigin: "50% 80%",
+			duration: singleDuration / 2,
+			delay: initDelay + singleDuration / 2,
+		})
+		.to(split.value.chars[1], { rotateX: 0, duration: singleDuration / 2 });
+};
+
+onMounted(() => {
+	split.value = SplitText.create(".details_title", { type: "chars" });
+	firstCharAnim();
+	secondCharAnim();
+});
+</script>
+
+<template>
+	<div class="details_title">WALCOME</div>
+</template>
+
+<style scoped lang="scss">
+.details_title {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100dvh;
+	width: 100%;
+	font-size: 5rem;
+}
+</style>
--- app/pages/details.vue
+++ app/pages/details.vue
@@ -1,19 +1,52 @@
 <script setup lang="ts">
+import gsap from "gsap";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+
+import Button from "~/components/ui/common/Button.vue";
+import DetailTitle from "~/components/ui/detailUI/DetailTitle.vue";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const loadingStore = useLoadingStore();
+const smoother = ref<ScrollSmoother | null>(null);
+
 usePageReady();
 
 onMounted(() => {
+	smoother.value = ScrollSmoother.create({
+		wrapper: ".details",
+		content: ".details_container",
+		smooth: 1,
+	});
 	document.title = "个人介绍";
 });
 </script>
 
 <template>
-	<div class="details"></div>
+	<div class="details">
+		<div class="details_container">
+			<DetailTitle />
+		</div>
+		<Button
+			:text="'back'"
+			:icon="'&#xeb06;'"
+			:size="'small'"
+			@click="loadingStore.loadingNavigate('/')"
+			:style="{ position: 'fixed', left: '20px', top: '20px' }"
+		></Button>
+	</div>
 </template>
 
 <style scoped lang="scss">
 .details {
 	height: 100dvh;
 	width: 100%;
+	font-size: 1rem;
 	background-color: red;
+
+	.details_container {
+		height: 500dvh;
+		width: 100%;
+	}
 }
 </style>
