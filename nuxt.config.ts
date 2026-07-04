aae6cc9f0f4f7c271ca5317638cbc9aab6ddb777
player32611 <hcb326630@163.com>
2026-07-04 16:43:00 +0800

    SSR 优化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		baseURL: "/blog-2.0/",
+	},
 	modules: ["@nuxt/content", "@pinia/nuxt", "@nuxt/fonts"],
 	devtools: { enabled: true },
 	compatibilityDate: "2024-04-03",
@@ -43,8 +46,9 @@ export default defineNuxtConfig({
 			},
 		],
 	},
-	ssr: false,
-	app: {
-		baseURL: "/blog-2.0/",
+	nitro: {
+		prerender: {
+			crawlLinks: true,
+		},
 	},
 });
