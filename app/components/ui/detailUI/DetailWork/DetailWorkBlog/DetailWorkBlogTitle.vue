d9eb4e1563905cb8719224856b72554e12482220
player32611 <hcb326630@163.com>
2026-07-14 13:45:51 +0800

    标题动画

--- app/components/ui/detailUI/DetailWork/DetailWorkBlog/DetailWorkBlogTitle.vue
+++ app/components/ui/detailUI/DetailWork/DetailWorkBlog/DetailWorkBlogTitle.vue
@@ -1,13 +1,38 @@
 <script setup lang="ts">
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
 
-onMounted(() => {});
+gsap.registerPlugin(SplitText);
 
-onUnmounted(() => {});
+const contentRef = ref<HTMLDivElement | null>(null);
+const contentSplit = ref<SplitText | null>(null);
+const mountAnim = ref<GSAPTween | null>(null);
+
+const animDelay = 1;
+
+onMounted(() => {
+	contentSplit.value = SplitText.create(contentRef.value, { type: "chars" });
+	mountAnim.value = gsap.from(contentSplit.value.chars, {
+		duration: 1,
+		opacity: 0,
+		scale: 0,
+		y: 80,
+		rotationX: 180,
+		transformOrigin: "0% 50% -50",
+		ease: "back",
+		delay: animDelay,
+		stagger: 0.05,
+	});
+});
+
+onUnmounted(() => {
+	contentSplit.value?.revert();
+	mountAnim.value?.kill();
+});
 </script>
 
 <template>
-	<div class="work_blog_title">title</div>
+	<div class="work_blog_title" ref="contentRef">PERSONAL WEBSITE</div>
 </template>
 
 <style scoped lang="scss">
