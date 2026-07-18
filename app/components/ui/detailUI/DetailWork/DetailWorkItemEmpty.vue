e6fd99fc30bb3cfa19a36603e00285a4367150a8
player32611 <hcb326630@163.com>
2026-07-18 20:41:04 +0800

    移除 Bubble

--- app/components/ui/detailUI/DetailWork/DetailWorkItemEmpty.vue
+++ app/components/ui/detailUI/DetailWork/DetailWorkItemEmpty.vue
@@ -1,56 +1,7 @@
-<script setup lang="ts">
-import gsap from "gsap";
-
-import Bubble from "~/components/exhibit/Bubble.vue";
-
-const bubbleRefs = ref<HTMLDivElement[] | null>(null);
-
-const bubbleNum = 10;
-const moveDuration = 1;
-
-const handleClick = (event: MouseEvent) => {
-	bubbleRefs.value?.forEach(el => {
-		const rect = el.getBoundingClientRect();
-		if (
-			rect.x <= event.x &&
-			event.x <= rect.x + rect.width &&
-			rect.y <= event.y &&
-			event.y <= rect.y + rect.height
-		) {
-			const centerX = rect.x + rect.width / 2;
-			const centerY = rect.y + rect.height / 2;
-			gsap.to(el, {
-				left: `+=${(event.x < centerX ? 1 : -1) * (Math.random() * 150 + 50)}px`,
-				top: `+=${(event.y < centerY ? 1 : -1) * (Math.random() * 150 + 50)}px`,
-				ease: "power2.out",
-				duration: moveDuration,
-			});
-		}
-	});
-};
-
-onMounted(() => {
-	bubbleRefs.value?.forEach(el => {
-		gsap.set(el, {
-			left: Math.random() * window.innerWidth,
-			top: Math.random() * window.innerHeight,
-			zoom: Math.random() * 0.7 + 0.3,
-			animationDelay: Math.random() * -6,
-		});
-	});
-	window.addEventListener("click", handleClick);
-});
-
-onUnmounted(() => {
-	window.removeEventListener("click", handleClick);
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
 	<div class="work_empty">
-		<div class="bubble_container" v-for="_ in bubbleNum" ref="bubbleRefs">
-			<!-- <Bubble /> -->
-		</div>
 		<div class="empty_content_english">waiting</div>
 		<div class="empty_content_chinese">敬请期待</div>
 	</div>
@@ -68,43 +19,6 @@ onUnmounted(() => {
 	width: 100%; /* Fill the entire screen */
 	overflow: hidden;
 	background: radial-gradient(circle, rgba(255, 255, 255, 0.2), rgba(0, 0, 0, 0.1));
-
-	// &::before,
-	// &::after {
-	// 	content: "";
-	// 	position: absolute;
-	// 	top: 50%;
-	// 	left: 50%;
-	// 	width: 300%;
-	// 	height: 300%;
-	// 	background: conic-gradient(
-	// 		from 0deg,
-	// 		#ff9aa2,
-	// 		/* Soft pink */ #ffb7b2,
-	// 		/* Light peach */ #ffdac1,
-	// 		/* Pastel yellow */ #e2f0cb,
-	// 		/* Mint green */ #a2e4ff,
-	// 		/* Baby blue */ #c9afff,
-	// 		/* Lavender */ #ffb7b2,
-	// 		#ff9aa2
-	// 	);
-	// 	transform: translate(-50%, -50%);
-	// 	animation: rotate 8s linear infinite;
-	// 	filter: blur(50px); /* Create a soft glowing effect */
-	// 	opacity: 0.8;
-	// }
-
-	// &::after {
-	// 	animation: rotate-reverse 10s linear infinite;
-	// 	opacity: 0.6;
-	// }
-
-	.bubble_container {
-		position: absolute;
-		height: 200px;
-		width: 200px;
-		animation: animate_4010 8s ease-in-out infinite;
-	}
 
 	.empty_content_english {
 		color: #ffffff;
@@ -135,35 +49,11 @@ onUnmounted(() => {
 	}
 }
 
-@keyframes rotate {
-	0% {
-		transform: translate(-50%, -50%) rotate(0deg);
-	}
-	100% {
-		transform: translate(-50%, -50%) rotate(360deg);
-	}
-}
-
-@keyframes rotate-reverse {
-	0% {
-		transform: translate(-50%, -50%) rotate(0deg);
-	}
-	100% {
-		transform: translate(-50%, -50%) rotate(-360deg);
-	}
-}
-
 /* ========== 超小屏（< 576px）========== */
 @media screen and (max-width: 576px) {
 	$base-size: 0.5;
 
 	.work_empty {
-		.bubble_container {
-			position: absolute;
-			height: 200px * $base-size;
-			width: 200px * $base-size;
-		}
-
 		.empty_content_english {
 			font-size: 5rem * $base-size;
 			text-shadow: 0px 0px 10px * $base-size rgba($color: #ff7f27, $alpha: 0.8);
@@ -193,12 +83,6 @@ onUnmounted(() => {
 	$base-size: 0.65;
 
 	.work_empty {
-		.bubble_container {
-			position: absolute;
-			height: 200px * $base-size;
-			width: 200px * $base-size;
-		}
-
 		.empty_content_english {
 			font-size: 5rem * $base-size;
 			text-shadow: 0px 0px 10px * $base-size rgba($color: #ff7f27, $alpha: 0.8);
@@ -228,12 +112,6 @@ onUnmounted(() => {
 	$base-size: 0.8;
 
 	.work_empty {
-		.bubble_container {
-			position: absolute;
-			height: 200px * $base-size;
-			width: 200px * $base-size;
-		}
-
 		.empty_content_english {
 			font-size: 5rem * $base-size;
 			text-shadow: 0px 0px 10px * $base-size rgba($color: #ff7f27, $alpha: 0.8);
@@ -263,12 +141,6 @@ onUnmounted(() => {
 	$base-size: 0.9;
 
 	.work_empty {
-		.bubble_container {
-			position: absolute;
-			height: 200px * $base-size;
-			width: 200px * $base-size;
-		}
-
 		.empty_content_english {
 			font-size: 5rem * $base-size;
 			text-shadow: 0px 0px 10px * $base-size rgba($color: #ff7f27, $alpha: 0.8);
